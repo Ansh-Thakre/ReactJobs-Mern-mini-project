e208074288fb3ac3a97e527ec81db479203df9e9
Ansh-Thakre <anshthakre013@gmail.com>
2026-01-31 01:39:26 +0530

    after deployment of frontend again

--- Backend/Server.js
+++ Backend/Server.js
@@ -14,7 +14,7 @@ connectDB();
 //after deployment
 app.use(
   cors({
-    origin: "https://anshreactminiapp.netlify.app/",
+    origin: "https://anshreactminiapp.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
